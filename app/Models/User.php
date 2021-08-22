3b1d421509979b5f9222007d946e9ce3dbf05192
José Guerrero <jose.guerrero.carrizo@gmail.com>
2021-08-22 10:29:36 -0500

    Laravel Passport installation and configurations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static find(int $id)
+ * @method paginate()
+ * @property mixed id
+ * @property mixed name
+ * @property mixed email
+ * @property mixed email_verified
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
