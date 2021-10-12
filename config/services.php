b17e4ee0151b316bf72d03364713279019d90279
José Guerrero <jose.guerrero.carrizo@gmail.com>
2021-10-11 19:05:48 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit acd505c919cbf174593cb66c5febffb6da71fb78.

--- config/services.php
+++ config/services.php
@@ -14,12 +14,6 @@ return [
     |
     */
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>  env('GOOGLE_CLIENT_REDIRECT'),
-    ],
-
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +15,4 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/auth/google', function () {
-    return Socialite::driver('google')->redirect();
-});
-
-Route::get('/auth/google/callback', function () {
-    $user = Socialite::driver('google')->user();
-
-    dd($user->token);
 });
